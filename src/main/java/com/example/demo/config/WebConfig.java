60e8bdde55c1a80a4818b45117a0162eb9faad6a
iamlamino <ply_thiam@hotmail.com>
2025-10-02 11:20:00 +0000

    added web configs for deployment permissions

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // toutes les routes
+                .allowedOrigins(
+                        "http://localhost:4200"      // ton Angular local
+                        //"https://ton-front.vercel.app" // ton front déployé sur Vercel
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
